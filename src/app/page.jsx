35c1013134cde2195f181756fbb98d88659eeba3
RosanneVN <annevazquez05@gmail.com>
2024-03-08 12:26:36 -0500

    fix redirect funtion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,23 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
-export default async function  Home() {
-  setTimeout(() => {
-    redirect("./about")
-  }, 3000);
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Redirigir a la página deseada
+      router.push("/about");
+    }, 5000); // 5 segundos
+
+    // Limpiar el temporizador al desmontar el componente
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-  <main className="bg-[#e5e6e7] h-[100vh] w-full flex justify-center items-center overflow-hidden" >
-    <div class="card">
+    <main className="bg-[#e5e6e7] h-[100vh] w-full flex justify-center items-center overflow-hidden">
+      <div class="card">
         <div class="circle"></div>
         <div class="circle"></div>
         <div class="circle"></div>
@@ -18,7 +28,7 @@ export default async function  Home() {
         <div class="card-inner backdrop-blur-sm flex justify-center items-center">
           <h1 className="text-3xl text-[#362966] font-bold z-50">Bubble</h1>
         </div>
-    </div> 
-  </main>
+      </div>
+    </main>
   );
 }
