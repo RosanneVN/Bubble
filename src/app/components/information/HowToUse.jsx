a836b086d6f1a59f328f4c7c35e0339e90ff6abf
RosanneVN <annevazquez05@gmail.com>
2024-04-19 20:10:50 -0400

    create product "how to use" component

---
+++ src/app/components/information/HowToUse.jsx
@@ -0,0 +1,21 @@
+export default function HowToUse() {
+  return (
+    <>
+      <section className="flex flex-col w-full h-[40vh] px-5 gap-2 text-[15px]">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          fugiat aut magni ut perferendis! Quidem id ex molestiae totam aliquid
+          veniam, laborum ipsum accusantium eligendi consequatur temporibus,
+          exercitationem debitis hic.
+        </p>
+        <p className=" font-semibold">No hacer:</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          quam nulla laborum nemo natus rerum dolorem recusandae optio odio
+          reprehenderit aspernatur atque asperiores ea voluptatum facere
+          cupiditate nam id porro!
+        </p>
+      </section>
+    </>
+  );
+}
