5c884b4bebaaf4d5f7dfe5a54989f4b8c0c515a9
RosanneVN <annevazquez05@gmail.com>
2024-04-19 20:11:22 -0400

    create product ingredients component

---
+++ src/app/components/information/Ingredients.jsx
@@ -0,0 +1,29 @@
+export default function Ingredients() {
+  return (
+    <>
+      <section className="flex flex-col w-full h-[40vh] px-5 gap-2">
+        <div>
+        
+          <p className=" font-semibold">Actives:</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div>
+            <p className="text-[15px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium incidunt ullam ab, totam autem itaque voluptatum
+              laboriosam sunt molestias, non vero! Nemo, aliquam dolorum.
+            </p>
+          </div>
+          <div>
+         
+            <p className="text-[15px]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+              quisquam unde voluptatem, sequi quas perspiciatis non, nemo quam
+              vitae veniam expedita ut optio impedit aliquam.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
