60e3a73937049997b6aefdee4e3cb718fe40aef3
RosanneVN <annevazquez05@gmail.com>
2024-04-22 21:35:06 -0400

    create a state for change the products page components

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,16 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
 import Link from "next/link";
 import BackButton from "../components/buttons/BackButton";
 import Details from "../components/information/Details";
 import Ingredients from "../components/information/Ingredients";
 import HowToUse from "../components/information/HowToUse";
+import { useState } from "react";
+
 
 export default function Product() {
-c
+  const [currentComponent, setCurrentComponent] = useState("details");
+
+  const handleClick = (newCurrentComponent) => {
+    setCurrentComponent(newCurrentComponent);
+  };
 
   return (
     <main className="w-full h-[100vh] flex flex-col ">
-
       <section className=" w-full flex justify-center items-center relative h-16">
         <BackButton url={"/routine"}></BackButton>
         <div className=" h-6 absolute left-24 top-7">
@@ -24,19 +30,22 @@ c
           <img className=" h-52 w-60 " src="/image/cleaneser.png" alt="" />
         </div>
       </section>
-      <section>
-        <div className="font-bold text-[13px] flex justify-around my-7 text-gray-500">
-          <button>DETAILS</button>
-          <button>INGREDIENTS</button>
-          <button>HOW TO USE</button>
+      <section className="text-[#4e4c57]">
+        <div className="font-bold text-[13px] flex justify-around my-5 text-gray-400">
+          <button onClick={() => handleClick("details")} className={currentComponent === "details" && "text-black"}>DETAILS</button>
+          <button onClick={() => handleClick("ingredients")} className={currentComponent === "ingredients" && "text-black"}>
+            INGREDIENTS
+          </button>
+          <button onClick={() => handleClick("howtouse")} className={currentComponent === "howtouse" && "text-black"}>HOW TO USE</button>
         </div>
-
-       <Details></Details>
+        {currentComponent === "details" && <Details></Details> }
+        {currentComponent === "ingredients" && <Ingredients></Ingredients>}
+        {currentComponent === "howtouse" && <HowToUse></HowToUse>}
       </section>
 
       <section className="flex justify-center items-center">
         <div
-          className="flex flex-row justify-center items-center my-6 bg-[#beb6de] hover:bg-[#362966] 
+          className="flex flex-row justify-center items-center my-7 bg-[#beb6de] hover:bg-[#362966] 
          w-[90%] rounded-full gap-3 py-3"
         >
           <Link href="/product" className="  text-center">
