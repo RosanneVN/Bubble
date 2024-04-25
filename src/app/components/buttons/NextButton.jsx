e186cbd9bcceefa2e761b2d82aa7a0caa39e4061
RosanneVN <annevazquez05@gmail.com>
2024-04-25 16:02:06 -0400

    fix the next button component

--- src/app/components/buttons/NextButton.jsx
+++ src/app/components/buttons/NextButton.jsx
@@ -4,15 +4,14 @@ export default function NextButton({ url }) {
   return (
     <>
       <section className="flex justify-center items-center ">
-        <div
+        <Link
+          href={url}
           className="flex flex-row justify-center items-center my-6 bg-[#beb6de] hover:bg-[#362966] 
          w-[90%] rounded-full gap-3 py-3"
         >
-          <Link href={url} className=" font-semibold text-center">
-            Next
-          </Link>
+          <p className=" font-semibold text-center"> Next</p>
           <img className="w-5 h-5" src="/icons/next.png" alt="" />
-        </div>
+        </Link>
       </section>
     </>
   );
