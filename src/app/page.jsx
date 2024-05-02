277ecad73d583734901e3ce73baef3cb9fb02a01
RosanneVN <annevazquez05@gmail.com>
2024-05-02 16:39:51 -0400

    create "we're not a doctor" banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
 import Nav from "./components/landing/Nav";
+import StartButton from "./components/landing/StartButton";
 
 export default function Home() {
   return (
-    <main className="px-20 py-10 text-xl flex flex-col xl:gap-52 md:gap-40 sm:gap-20 2xl:gap-60">
+    <main className="px-20 py-10 text-xl flex flex-col text-gray-500 xl:gap-52 md:gap-40 sm:gap-20 2xl:gap-60">
       <div>
         <Nav></Nav>
         <section className="flex flex-row pt-20 px-10">
-          <div className="flex flex-col gap-10 w-[50%]">
+          <div className="flex flex-col gap-10 w-[50%] justify-center">
             <p className="text-5xl font-bold text-[#362966]">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elituibusdam.
+              Find your skincare routine.
             </p>
             <div className="flex flex-col gap-7">
               <p>
@@ -17,14 +18,10 @@ export default function Home() {
                 Commodi cum dicta necessitatibus est eum at accusamus soluta
                 repellendus impedit tempore.
               </p>
-              <p className="text-blue-800">
-                Find your perfect skincare routine!
-              </p>
-              <div className="flex m-auto">
-                <button className="bg-[#beb6de] py-3 px-10 rounded-3xl hover:translate-y-1 w-fit shadow-lg text-base">
-                  Start now
-                </button>
-              </div>
+
+              <button className="bg-[#beb6de] py-3 px-10 rounded-3xl hover:translate-y-1 w-fit shadow-lg text-base">
+                Start now
+              </button>
             </div>
           </div>
           <div className="w-[50%] flex justify-center items-center">
@@ -39,7 +36,7 @@ export default function Home() {
       <section className="flex flex-row px-10">
         <div className="w-[50%]">
           <img
-            className="w-[450px] h-[500px] "
+            className="w-[450px] h-[500px]"
             src="/image/michi_dev.jpg"
             alt=""
           />
@@ -59,6 +56,26 @@ export default function Home() {
           </p>
         </div>
       </section>
+      <section className="relative">
+        <div className="absolute -top-11 -left-8">
+          <img className="h-20 w-20" src="icons/advertencia.png" alt="" />
+        </div>
+        <div className="bg-[#e5e6e7] rounded-2xl py-10 px-5">
+          <p className="font-bold text-[#362966] text-center text-2xl">
+            We are not a doctor
+          </p>
+          <p className="pt-5">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel quae,
+            reprehenderit odit et facere in consequuntur laboriosam magni enim
+            expedita quasi, quaerat quo, cum alias inventore culpa dolorum
+            doloremque autem ipsum dolor, sit amet consectetur adipisicing elit.
+            Vel quae, reprehenderit odit et facere in consequuntur laboriosam
+            magni enim expedita quasi, quaerat quo, cum alias inventore culpa
+            dolorum doloremque autem!
+          </p>
+        </div>
+      </section>
+      
     </main>
   );
 }
