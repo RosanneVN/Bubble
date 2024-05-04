a6245d49ef385a2dd5b32d4cf63a7e4d4d52731a
RosanneVN <annevazquez05@gmail.com>
2024-05-03 20:04:00 -0400

    create the social button

---
+++ src/app/components/landing/SocialButton.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function SocialButton({ color, icon, url }) {
+  return (
+    <>
+      <Link
+        className={` ${color} rounded-[50%] w-14 h-14 flex justify-center items-center`}
+        href={url}
+      >
+        <img className="size-7 hover:size-9" src={icon} alt="" />
+      </Link>
+    </>
+  );
+}
