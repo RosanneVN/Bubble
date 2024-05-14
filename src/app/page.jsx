a92770d4b288ee17a78e7616345d2988d0eed9e2
RosanneVN <annevazquez05@gmail.com>
2024-05-14 11:27:06 -0400

    update main page (landing)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,19 +5,25 @@ import Nav from "./components/landing/Nav";
 import Screenshots from "./components/landing/Screenshots";
 import ContactUs from "./components/landing/ContactUs";
 import FeaturesSection from "./components/landing/FeaturesSection";
+import Footer from "./components/landing/Footer";
 
 export default function Home() {
   return (
-    <main className="px-20 py-10 text-xl flex flex-col text-gray-500 xl:gap-52 md:gap-40 sm:gap-20 2xl:gap-60">
-      <div>
+    <main className="pt-10 text-xl flex flex-col text-gray-500 xl:gap-52 md:gap-40 sm:gap-20 2xl:gap-60 items-center">
+      <div className="px-20 max-w-[1440px]">
         <Nav></Nav>
         <Hero></Hero>
       </div>
-      <IAInfo></IAInfo>
+      <div className="px-20 max-w-[1440px]">
+        <IAInfo></IAInfo>
+      </div>
       <DangerBanner></DangerBanner>
-      <FeaturesSection></FeaturesSection>
-      <Screenshots></Screenshots>
-      <ContactUs></ContactUs>
+      <div className="flex flex-col px-20 xl:gap-52 md:gap-40 sm:gap-20 2xl:gap-60 max-w-[1440px]">
+        <FeaturesSection></FeaturesSection>
+        <Screenshots></Screenshots>
+        <ContactUs></ContactUs>
+      </div>
+      <Footer></Footer>
     </main>
   );
 }
