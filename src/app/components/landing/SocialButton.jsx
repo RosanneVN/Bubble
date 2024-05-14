180eefd63d3647f31a5923d046d00b98d1f1cd15
RosanneVN <annevazquez05@gmail.com>
2024-05-14 11:25:33 -0400

    create Social Btn component

--- src/app/components/landing/SocialButton.jsx
+++ src/app/components/landing/SocialButton.jsx
@@ -4,7 +4,7 @@ export default function SocialButton({ color, icon, url }) {
   return (
     <>
       <Link
-        className={` ${color} rounded-[50%] w-14 h-14 flex justify-center items-center`}
+        className={` ${color} rounded-[50%] size-14 flex justify-center items-center`}
         href={url}
       >
         <img className="size-7 hover:size-9" src={icon} alt="" />
